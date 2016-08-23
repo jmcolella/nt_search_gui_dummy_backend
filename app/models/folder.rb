73ef26209449783cefaca80cd501ba3808b53e09
John Colella <colella.john@gmail.com>
2016-08-23 16:27:18 -0400

    set up seed file of dummy directory for database

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,8 +1,9 @@
 class Folder < ApplicationRecord
 
-  has_many :documents
+  belongs_to :partition, optional: true
   has_many :sub_folders, class_name: "Folder", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Folder", foreign_key: "parent_id", optional: true
+  has_many :documents
 
   validates :name, uniqueness: true
 end
